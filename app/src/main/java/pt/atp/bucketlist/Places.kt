883ae96b2502e8745d7d1f3448d22ed254408023
Inês Soares <up201606615@fe.up.pt>
2021-02-09 19:02:42 +0000

    added visited places

--- app/src/main/java/pt/atp/bucketlist/Places.kt
+++ app/src/main/java/pt/atp/bucketlist/Places.kt
@@ -11,7 +11,33 @@ object Places {
     )
 
     val placesVisited = listOf(
+        Place("Italy - Venice", R.drawable.venice),
+        Place("Poland - Wroclaw", R.drawable.wroclaw),
+        Place("Poland - Auschwitz", R.drawable.auschwitz),
+        Place("Poland - Zakopane", R.drawable.zakopane),
+        Place("Germany - Berlin", R.drawable.berlin),
+        Place("Austria - Salzburg", R.drawable.salzburg),
+        Place("Germany - Munich", R.drawable.munich),
+        Place("Germany - Dresden", R.drawable.dresden),
+        Place("Check Republic - Prague", R.drawable.prague),
+        Place("Hungary - Budapest", R.drawable.budapest),
+        Place("Poland - Warsaw", R.drawable.warsaw),
+        Place("Norway - Oslo", R.drawable.oslo),
+        Place("Poland - Gdansk", R.drawable.gdansk),
+        Place("Poland - Poznan", R.drawable.poznan),
+        Place("Poland - Krakow", R.drawable.krakow),
+        Place("Germany - Leipzig", R.drawable.leipzig),
+        Place("The Netherlands - Utrecht", R.drawable.utrecht),
+        Place("The Netherlands - Amsterdam", R.drawable.amsterdam),
+        Place("The Netherlands - Rotterdam", R.drawable.rotterdam),
+        Place("Belgium - Gent", R.drawable.gent),
+        Place("Belgium - Brugge", R.drawable.brugge),
+        Place("Belgium - Brussels", R.drawable.brussels),
+        Place("Belgium - Antwerp", R.drawable.antwerp),
         Place("USA - Route 66", R.drawable.usa),
+        Place("Switzerland", R.drawable.switzerland),
+        Place("France - Paris", R.drawable.paris),
+        Place("UK - London", R.drawable.london),
     )
 
 }
--- app/src/main/java/pt/atp/bucketlist/fragments/VisitedFragment.kt
+++ app/src/main/java/pt/atp/bucketlist/fragments/VisitedFragment.kt
@@ -5,12 +5,33 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import pt.atp.bucketlist.Places
 import pt.atp.bucketlist.R
+import pt.atp.bucketlist.model.FeedAdapter
+import pt.atp.bucketlist.model.StoryAdapter
 
 class VisitedFragment: Fragment(R.layout.fragment_visited)  {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val rootView: View = inflater.inflate(R.layout.fragment_visited,container,false)
+
+        val stories: RecyclerView = rootView.findViewById(R.id.rv_stories)
+        val feed: RecyclerView = rootView.findViewById(R.id.rv_feed)
+
+        stories.apply {
+            setHasFixedSize(true)
+            layoutManager = LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
+            adapter = StoryAdapter(Places.placesVisited)
+        }
+
+        feed.apply {
+            setHasFixedSize(true)
+            layoutManager = LinearLayoutManager(context)
+            adapter = FeedAdapter(Places.placesVisited)
+        }
+
         return rootView
     }
 
